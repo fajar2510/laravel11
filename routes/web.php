0bacc5b5a10eb005cc798bc6407bd4246e72fa55
Fajar Abdurrohman <sepertiseharusnyafajar@gmail.com>
2025-01-06 07:38:29 +0700

    route-article-slug

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,75 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home');
+    return view('home',['title' =>"Home Page"]);
 });
 
 Route::get('/about', function () {
-    return view('about', ['name' => 'Nicole Rossi ']);
+    return view('about', ['title' => 'About']);
 });
 
-Route::get('/blog', function () {
-    return view('blog');
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog', 
+    'posts' => [
+                [
+                    'id' => '1',
+                    'slug' => 'judul-artikel-1',
+                    'title' => 'Judul Artikel 1',
+                    'author' => 'Nicole Rossi',
+                    'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                            Minus, veritatis ad soluta harum deleniti totam doloremque odit obcaecati facilis earum!. 
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Id',
+                    'date' => '6 January, 2025'
+                ],
+                [
+                    'id' => '2',
+                    'slug' => 'judul-artikel-2',
+                    'title' => 'Judul Artikel 2',
+                    'author' => 'Fajar Abdurrohman',
+                    'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                            Officia animi ad distinctio facilis eos ipsam, exercitationem culpa sapiente aliquid. 
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nemo?',
+                    'date' => '5 January, 2025'
+                ],
+               ]  
+          ]);
+});
+
+Route::get('/posts/{slug}', function ($slug) {
+    $posts = [
+        [
+            'id' => '1',
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul Artikel 1',
+            'author' => 'Nicole Rossi',
+            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+                    Minus, veritatis ad soluta harum deleniti totam doloremque odit obcaecati facilis earum!. 
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Id',
+            'date' => '6 January, 2025'
+        ],
+        [
+            'id' => '2',
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul Artikel 2',
+            'author' => 'Fajar Abdurrohman',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Officia animi ad distinctio facilis eos ipsam, exercitationem culpa sapiente aliquid. 
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, nemo?',
+            'date' => '5 January, 2025'
+        ],
+    ];
+
+    $post = Arr::first($posts, function ($post) use ($slug){
+        return $post['slug'] == $slug;
+    });
+
+    // dd($post);
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/contact', function () {
-    return view('contact');
+    return view('contact', ['title' => 'Contact']);
 });
